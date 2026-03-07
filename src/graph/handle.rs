3e8076d5e7478faca06ae73c25e0317befca9db2
Buğra Kadirhan <bugrakadirhan@proton.me>
2026-03-07 14:00:06 +0300

    Revert "Removed Checked<T>, for future redesign"
    This reverts commit 147861adcaeeac832c76ddee2bff3559a8b21820.

--- src/graph/handle.rs
+++ src/graph/handle.rs
@@ -91,3 +91,32 @@ impl Handle for EdgeHandle {
         self.graph()
     }
 }
+
+#[derive(Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash, Debug)]
+pub struct Checked<T: Handle>(T);
+
+impl<T: Handle> Checked<T> {
+    pub fn generation(handle: T) -> Option<Self> {
+        if handle.generation() == 1 {
+            return Some(Self(handle));
+        }
+
+        None
+    }
+
+    pub fn graph_and_generation(handle: T, graph: u32) -> Option<Self> {
+        if handle.graph() == graph {
+            return Self::generation(handle);
+        }
+
+        None
+    }
+
+    pub fn index(&self) -> usize {
+        self.0.index()
+    }
+
+    pub fn into_inner(self) -> T {
+        self.0
+    }
+}
