04b8221cf55447abb1f15541606a639074fd9cb1
Buğra Kadirhan <bugrakadirhan@proton.me>
2026-03-07 16:09:54 +0300

    Move fundamental parts to core, split off BasicGraph. Better naming!

--- src/graph/core/mod.rs
+++ src/graph/core/mod.rs
@@ -1,5 +1,9 @@
 mod checked;
+mod graph;
+mod graph_ext;
 mod handle;
 
 pub use checked::Checked;
+pub use graph::{Graph, GraphMut};
+pub use graph_ext::*;
 pub use handle::{EdgeHandle, Handle, VertexHandle};
--- src/graph/mod.rs
+++ src/graph/mod.rs
@@ -1,15 +1,13 @@
 mod algorithm;
+mod basic_graph;
 mod core;
-mod graph_ext;
-mod graph_trait;
 mod property_map;
 mod storage;
 mod topology;
 
 pub use algorithm::*;
+pub use basic_graph::BasicGraph;
 pub use core::*;
-pub use graph_ext::*;
-pub use graph_trait::{BasicGraph, Graph, GraphMut};
 pub use property_map::*;
 pub use storage::*;
 pub use topology::*;
