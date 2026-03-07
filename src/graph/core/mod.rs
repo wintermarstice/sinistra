b8fec48d1abd876b31ad6f68b9b8b8ee6e1a8f98
Buğra Kadirhan <bugrakadirhan@proton.me>
2026-03-07 16:03:44 +0300

    Create core module in graph, move handle/checked there

---
+++ src/graph/core/mod.rs
@@ -0,0 +1,5 @@
+mod checked;
+mod handle;
+
+pub use checked::Checked;
+pub use handle::{EdgeHandle, Handle, VertexHandle};
--- src/graph/mod.rs
+++ src/graph/mod.rs
@@ -1,15 +1,15 @@
 mod algorithm;
+mod core;
 mod graph_ext;
 mod graph_trait;
-mod handle;
 mod property_map;
 mod storage;
 mod topology;
 
 pub use algorithm::*;
+pub use core::*;
 pub use graph_ext::*;
 pub use graph_trait::{BasicGraph, Graph, GraphMut};
-pub use handle::{Checked, EdgeHandle, Handle, VertexHandle};
 pub use property_map::*;
 pub use storage::*;
 pub use topology::*;
